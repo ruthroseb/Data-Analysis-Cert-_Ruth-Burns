09e725ab04434a49074f17dce6ccad44cf5ae2ed
ruthroseb <ruthroseburns@gmail.com>
2021-06-19 17:05:28 +0100

    Matplotlib Food product charts

--- Film Analysis.py
+++ Film Analysis.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import numpy as np
+import matplotlib
 
 # API (omdb api key is 87c8a7a3)
 r_harrypotter=requests.get('http://www.omdbapi.com/?apikey=87c8a7a3&t=harry+potter&plot=full')
@@ -18,10 +19,12 @@ print(r_harrypotter.json()["Ratings"])
 
 # looping a dictionary
 x=r_harrypotter.json()
+y=r_lordoftherings.json()
 for key, value in x.items():
     print(key + ": " + str(value))
 
+df_harrypotter = pd.DataFrame.from_dict(x, orient='index')
+df_lordoftherings = pd.DataFrame.from_dict(y, orient='index')
 
-
-
-
+concat_data=pd.concat([df_harrypotter,df_lordoftherings], axis=1)
+print(concat_data)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # import a CSV file into a Pandas DataFrame
 data = pd.read_csv('Food_Production.csv')
@@ -28,15 +29,78 @@ print(type(Total_Emissions_descending))
 data_total_emissions=data.loc[:,['Food product', 'Total_emissions']]
 Total_Emissions_descending=data_total_emissions.sort_values("Total_emissions", ascending=False)
 animal_products=data.loc[data['Animal Feed']!=0]
+animal_products_descending=animal_products.sort_values("Total_emissions", ascending=False)
 print(animal_products)
+animal_products_mean=animal_products['Total_emissions'].mean()
+
+#processed products
 processed_products=data.loc[data['Processing']!=0]
 print(processed_products)
-
+nonprocessed_products=data.loc[data['Processing']==0]
+print(nonprocessed_products)
+process_Products_mean=processed_products['Total_emissions'].mean()
+nonprocessed_products_mean=nonprocessed_products['Total_emissions'].mean()
+print(process_Products_mean)
 
 print(Total_Emissions_descending)
 print(data.loc[:5,['Food product','Packging', 'Total_emissions']])
 
-
 for lab, row in data.iterrows():
     print(lab)
     print(row)
+
+#oil products
+oils=data.loc[[15,16,17,18,19],]
+oils_desc=oils.sort_values('Total_emissions',ascending=False)
+print(oils_desc)
+oils_mean=oils['Total_emissions'].mean()
+
+#fruits
+fruits=data.loc[[27,25,30,26,28],]
+fruits_mean=fruits['Total_emissions'].mean()
+
+#vegetables
+vegetables=data.loc[[21,5,22,24,10,],]
+vegetables_mean=vegetables['Total_emissions'].mean()
+
+fig, ax = plt.subplots()
+plt.xticks(rotation=45)
+ax.plot(oils_desc['Food product'],oils_desc['Total_emissions'],marker='o',linestyle='None', color='y')
+ax.bar(oils_desc['Food product'], oils_desc['Land use change'],label='Land use change')
+ax.bar(oils_desc['Food product'], oils_desc['Farm'],label='Farm')
+ax.bar(oils_desc['Food product'], oils_desc['Processing'],label='Processing')
+ax.bar(oils_desc['Food product'], oils_desc['Packging'],label='Packging')
+ax.set_title('Total Emissions and impacting variables by Oil Type')
+plt.legend()
+
+fig, ax = plt.subplots()
+plt.xticks(rotation=45)
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Animal Feed'],label="Animal Feed")
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Land use change'],label='Land use change')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Animal Feed'],label='Animal Feed')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Farm'],label='Farm')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Processing'],label='Processing')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Transport'],label='Transport')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Packging'],label='Packging')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Retail'],label='Retail')
+ax.set_title('Animal Food Product Environmental impact by variables')
+plt.legend()
+
+
+fig, ax = plt.subplots()
+plt.xticks(rotation=45)
+x=("Animal Products", "Oil Products", "Processed Products", "Non Processed Products", "Fruits", "Vegetables")
+y = (animal_products_mean, oils_mean, process_Products_mean, nonprocessed_products_mean, fruits_mean,vegetables_mean)
+ax.plot(x,y,marker='o',linestyle='None', color='y')
+
+
+fig, ax = plt.subplots()
+plt.xticks(rotation=45)
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Eutrophying emissions per 1000kcal (gPO₄eq per 1000kcal)'], label="Eutrophying emissions")
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Freshwater withdrawals per 1000kcal (liters per 1000kcal)'],bottom=animal_products_descending['Eutrophying emissions per 1000kcal (gPO₄eq per 1000kcal)'],label='Freshwater withdrawals')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Greenhouse gas emissions per 1000kcal (kgCO₂eq per 1000kcal)'],bottom=animal_products_descending['Freshwater withdrawals per 1000kcal (liters per 1000kcal)'],label='Greenhouse gas emissions')
+ax.bar(animal_products_descending['Food product'], animal_products_descending['Land use per 1000kcal (m² per 1000kcal)'],bottom=animal_products_descending['Greenhouse gas emissions per 1000kcal (kgCO₂eq per 1000kcal)'],label='Land use')
+ax.set_title('Animal Food Product Environmental impact by variables (per 1000kcal)')
+plt.legend()
+
+plt.show()
